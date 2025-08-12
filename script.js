de91fe0556509a4b393d9185d100e12810dc6417
Chamara Wijepala <chamarawijepala00@gmail.com>
2025-08-12 13:18:13 +0530

    feat: add infinite loop

--- script.js
+++ script.js
@@ -4,7 +4,10 @@ const slide = document.querySelector('.slide');
 const leftBtn = document.querySelector('.left');
 const rightBtn = document.querySelector('.right');
 
-images.forEach((image) => {
+const leftClones = images.slice(-3);
+const rightClones = images.slice(0, 3);
+
+[leftClones, images, rightClones].flat().forEach((image) => {
 	const panel = document.createElement('div');
 	const panelImage = document.createElement('img');
 	panel.classList.add('panel');
@@ -14,11 +17,48 @@ images.forEach((image) => {
 	slide.append(panel);
 });
 
-const panelSize = slide.children[0]?.offsetWidth;
+let panelSize;
+
+window.addEventListener('load', () => {
+	panelSize = slide.children[0].offsetWidth;
+	// scroll past cloned left panels
+	slide.scrollBy({
+		top: 0,
+		left: panelSize * leftClones.length,
+		behavior: 'instant'
+	});
+})
+
+// prevent user from manually scrolling
+slide.addEventListener('wheel', (e) => e.preventDefault(), {passive: false});
+slide.addEventListener('touchmove', (e) => e.preventDefault(), {passive: false});
 
 leftBtn.addEventListener('click', () => {
+	leftBtn.disabled = true;
 	slide.scrollBy({ top: 0, left: -panelSize, behavior: 'smooth' });
 });
 rightBtn.addEventListener('click', () => {
+	rightBtn.disabled = true;
 	slide.scrollBy({ top: 0, left: panelSize, behavior: 'smooth' });
 });
+
+// scroll to correct panel to give user the illusion of a looping carousel 
+// Note: scrollend is not supported in some browsers
+slide.addEventListener('scrollend', () => {
+	leftBtn.disabled = false;
+	rightBtn.disabled = false;
+	if(slide.scrollLeft / panelSize === images.length - leftClones.length) {
+		slide.scrollBy({
+			top: 0,
+			left: images.length * panelSize,
+			behavior: 'instant'
+		})
+	}
+	if(slide.scrollLeft / panelSize === images.length + rightClones.length) {
+		slide.scrollBy({
+			top: 0,
+			left: images.length * -panelSize,
+			behavior: 'instant'
+		})
+	}
+})
